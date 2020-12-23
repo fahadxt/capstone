3af932d3d7d02ab653386e98ea2bc9a539cad7a6
fahadxt <f.s.almotery@gmail.com>
2020-12-23 06:00:07 +0300

    .

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-import unittest
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
@@ -10,18 +9,6 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
-@manager.command
-def run():
-    app.run()
-    
-@manager.command
-def test():
-    """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('test_app', pattern='test_app*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
 
 if __name__ == '__main__':
     manager.run()
